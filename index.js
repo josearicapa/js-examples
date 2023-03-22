30ba7d7d3db0e3e75f11255ddbe9128289097b04
josearicapa <54639293+josearicapa@users.noreply.github.com>
2023-03-22 12:43:58 -0500

    1 file modified

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const contador = () => {
 
   const incrementa = () => {
     numero++;
-    console.log(numero);
+    //console.log(numero);
   };
   return incrementa;
 };
@@ -17,3 +17,31 @@ const incrementar = contador();
 incrementar();
 incrementar();
 incrementar();
+
+// quitar duplicados de array
+const names = ['Jose', 'Jose', 'Luis', 'Luis'];
+
+const uniqueNames = names.reduce((acc, current) => {
+  !acc.includes(current) && acc.push(current);
+  return acc;
+}, []);
+
+const numbers = [2, 2];
+
+const total = numbers.reduce((sum, number) => sum + number, 0);
+
+console.log(uniqueNames);
+console.log(total);
+
+// Ejemplo de acceso al this
+const externalString = 'Luis';
+const object = {
+  name: 'Jose',
+  normalFunction: function () {
+    console.log(this.name);
+  },
+  arrowFunction: () => console.log(this.name, this),
+};
+
+object.normalFunction();
+object.arrowFunction();
